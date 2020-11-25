8b0c72da6d5b595cf62377d05f75c6979000c694
LifeExpress1 <aherweshall@gmail.com>
2020-11-25 16:33:32 +0530

    Add some code into existing projext

---
+++ src/com/vishal/GitClass.java
@@ -0,0 +1,10 @@
+package com.vishal;
+
+public class GitClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/com/vishal/TestGit.java
@@ -0,0 +1,7 @@
+package com.vishal;
+
+public class TestGit {
+	public static void main(String[] args) {
+		System.out.println("Hello Git from Eclipse");
+	}
+}
