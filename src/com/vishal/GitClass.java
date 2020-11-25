5d3ec1fa546d744c80427f3d0a8e17de1f6e1fcd
LifeExpress1 <aherweshall@gmail.com>
2020-11-25 16:48:46 +0530

    test commit updated GitClass file

--- src/com/vishal/GitClass.java
+++ src/com/vishal/GitClass.java
@@ -4,7 +4,9 @@ public class GitClass {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		for (int i = 0; i < 10; i++) {
+			System.out.println("Value " + i);
+		}
 	}
 
 }
