1cf426ca9a698ca32254f126c18a7e739277d632
LifeExpress1 <aherweshall@gmail.com>
2020-11-25 17:00:18 +0530

    Push changes

--- src/com/vishal/GitClass.java
+++ src/com/vishal/GitClass.java
@@ -4,7 +4,7 @@ public class GitClass {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		for (int i = 0; i < 10; i++) {
+		for (int i = 0; i < 5; i++) {
 			System.out.println("Value " + i);
 		}
 	}
---
+++ src/com/vishal/PushClass.java
@@ -0,0 +1,7 @@
+package com.vishal;
+
+public class PushClass {
+	public static void main(String[] args) {
+		System.out.println("Push");
+	}
+}
